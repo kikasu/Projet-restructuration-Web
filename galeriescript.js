06df087b20e0f97c7005509d711cb3eec6832be5
BrivaelFer <brivaelsunny@orange.fr>
2022-01-12 08:37:35 +0100

    first

---
+++ galeriescript.js
@@ -0,0 +1,30 @@
+//Entrer le text lier l'image, le nom de l'image, un entier point dans 'categori', puis l'années
+let imgAndText = [
+    ["t1","i", 0,0],  
+    ["t2","m",0,0]
+];
+let categorie = ["soleil, lune"];
+let year = [2008,2009,2010];
+function setImage(filtre1, filtre2)
+{
+
+}
+function setSFiltre(filtreP)
+{
+    let sFiltre = [];
+    switch (filtreP){
+        case 'soleil': sFiltre = [2008, 2009];
+            break;
+        case 'lune': sFiltre = [2008, 2010];
+            break;
+        
+    }
+    for(let i = 0; i < sFiltre.length; i++)
+    {
+
+    }
+}
+function setPageSelector(filtre1, filtre2)
+{
+
+}
