99769ebc5b77dbe07f3f7a65158b88e62cf165a8
Ilan-J <ilan.jaglin@viacesi.fr>
2022-01-17 16:46:13 +0100

    slide making

--- rsc-P-H/script.js
+++ rsc-P-H/script.js
@@ -11,6 +11,9 @@ function journalToggle(x) {
     }
 }
 
+
+
+let index_slide_photo_caron = 0;
 let serie_photo_caron = [
     './h.2.0-slide-halley1910.jpg',
     './h.2.1-slide-eclipse1912.jpg',
@@ -18,17 +21,30 @@ let serie_photo_caron = [
     './h.2.3-slide-cometemorehouse.png'
 ]
 
-function slide_photo_caron(img) {
-    console.log(img);
+function slide_photo_caron(x) {
+    console.log(x);
 
-    if (img == undefined) {
-        console.log('lol');
+    if (x == undefined) {
+        console.log('hide');
         // document.querySelector('#slide_photo_caron').classList.remove('active');
     }
+    else if (x == '<') {
+        console.log('prec');
+        index_slide_photo_caron--;
+
+        if (index_slide_photo_caron < 0) index_slide_photo_caron = serie_photo_caron.length -1;
+    }
+    else if (x == '>') {
+        console.log('next');
+        index_slide_photo_caron++;
+
+        if (index_slide_photo_caron > serie_photo_caron.length -1) index_slide_photo_caron = 0;
+    }
     else {
+        console.log('show');
+        index_slide_photo_caron = x;
         // document.querySelector('#slide_photo_caron').classList.add('active');
     }
-
-    document.querySelector('#slide_photo_caron_img').src = serie_photo_caron[img];
-    console.log(serie_photo_caron[img]);
+    document.querySelector('#slide_photo_caron_img').src = serie_photo_caron[index_slide_photo_caron];
+    console.log(index_slide_photo_caron, serie_photo_caron[index_slide_photo_caron]);
 }
